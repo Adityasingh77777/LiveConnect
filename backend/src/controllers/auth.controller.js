24fb1ae724600c314458e78a79ee8d94d13a6bdb
Adityasingh77777 <aditya.2201139ec@iiitbh.ac.in>
2025-08-18 04:13:21 +0530

    controllers added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,67 @@
-export const signup=(req,res)=>{
-    res.status("signup route")
-}
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+
+export const signup=async(req,res)=>{
+    const {fullName,email,password}=req.body;
+    try{
+        if(password.length < 6) {
+            return res.status(400).json({
+                message:"Password must be atleast 6 characters"
+            })
+        }
+
+        const user=await User.findOne({email});
+
+        if(user) return res.status(400).josn({
+            message:"Email already exists"
+        })
+
+        const hashedPassword=await bcrypt.hash(password,10);
+
+        const newUser=new User({
+            fullName,
+            email,
+            password:hashedPassword
+        })
+
+        if(newUser){
+            // generate jwt token
+             generateToken(newUser._id,res);
+             await newUser.save()
+
+             res.status(200).json({
+                message:"user created Successfully",
+                fullName:newUser.fullName,
+                email:newUser.email,
+                profilePic:newUser.profilePic
+             });
+        }
+
+        else{
+            return res.status(400).json({
+                message:"Invalid user credentials"
+            })
+        }
+    }
+    
+    catch(error){
+        console.log("Error in signup controller",error.message);
+        res.status(500).json({
+            message:"Registration failed"
+        })
+    }
+};
 
 export const login=(req,res)=>{
-    res.status("login route")
+   try{
+
+   }
+
+   catch(error){
+
+   }
 }
 
 export const logout=(req,res)=>{
-    res.status('logout route')
+    
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,8 @@ import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
+
+app.use(express.json());
 const PORT=process.env.PORT || 3000;
 
 app.use("/api/auth",authRoutes);
