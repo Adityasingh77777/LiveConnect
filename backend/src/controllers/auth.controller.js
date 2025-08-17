daf19b3b00f2bc5f9807263b0dc98b1f07f9fbf8
Adityasingh77777 <aditya.2201139ec@iiitbh.ac.in>
2025-08-18 02:38:09 +0530

    Backend Setup

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup=(req,res)=>{
+    res.status("signup route")
+}
+
+export const login=(req,res)=>{
+    res.status("login route")
+}
+
+export const logout=(req,res)=>{
+    res.status('logout route')
+}
