e109f910e8436dabada05f88a3e9c05a4adb5e23
Adityasingh77777 <aditya.2201139ec@iiitbh.ac.in>
2025-08-22 11:46:45 +0530

    Completed

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -8,8 +8,8 @@ export const generateToken=(userId,res) =>{
     res.cookie("jwt", token, {
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   httpOnly: true,
-  sameSite: "strict",                // allow cross-origin cookies
-  secure: process.env.NODE_ENV !== "development" // https required in prod
+  sameSite: "none",                // allow cross-origin cookies
+  secure: true // https required in prod
 });
 
 
