e860c4d9ab22c8f069ceb8840bab20c4c50977c7
Adityasingh77777 <aditya.2201139ec@iiitbh.ac.in>
2025-08-23 21:59:03 +0530

    all setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://live-connect-three.vercel.app"],
+    origin: ["http://localhost:5173","https://live-connect-three.vercel.app","https://liveconnect.pages.dev"],
     credentials: true,
   })
 );
